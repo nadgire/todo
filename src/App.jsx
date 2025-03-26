0b2b8e862f992f9253fa64e2c31a56d465e320e0
nadgire <abhinadgire@hotmail.com>
2025-03-27 04:24:16 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+import ResetPassword from './components/ResetPassword'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
