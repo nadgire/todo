c0ec3f6d5dd493be7a35e96523fb75f9c60e81d8
nadgire <abhinadgire@hotmail.com>
2025-03-28 12:42:10 +0530

    Login criteria

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom'
 import Login from './components/Login'
 import Register from './components/Register'
 import ResetPassword from './components/ResetPassword'
 import Footer from './components/Footer'
 import Dashboard from './components/Dashboard'
 
+function isTokenExpired(token) {
+  if (!token) return true;
+
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  const expirationTime = payload.exp * 1000;
+  return expirationTime < Date.now();
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const token = localStorage.getItem('token')
+
+  if (token && isTokenExpired(token)) {
+    localStorage.removeItem('token');
+    window.location.reload();
+    const navigate = useNavigate();
+
+    navigate('/')
+
+  }
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path="/" element={token && !isTokenExpired(token) ? <Navigate to="/dashboard" /> : <Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/dashboard" element={token && !isTokenExpired(token) ? <Dashboard /> : <Navigate to="/" />} />
         </Routes>
         <Footer />
       </BrowserRouter>
